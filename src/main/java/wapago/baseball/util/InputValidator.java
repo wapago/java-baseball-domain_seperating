9a2ef516cada1b2116efde88c6efb5b5601c235d
Kim Chanho <theanswerkk@naver.com>
2023-04-26 15:56:46 +0900

    [mod] 불필요변수삭제, 숫자입력체크 추가

--- src/main/java/wapago/baseball/util/InputValidator.java
+++ src/main/java/wapago/baseball/util/InputValidator.java
@@ -1,54 +1,45 @@
 package wapago.baseball.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import wapago.baseball.constant.BaseBallProperties;
 import wapago.baseball.controller.BaseBallController;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidator {
 	static BaseBallController baseBallController = new BaseBallController();
-	
-	private static String inputNumStr;
-	private static String[] inputNumberArr = new String[BaseBallProperties.LENGTH_OF_NUMBER];
-	private static List<String> inputNumStrList = new ArrayList<>();
-	private static Set<String>  inputNumStrSet = new HashSet<>(); 
+	private final static String NUMERIC = "^[1-9]+$";
 	
 	public static void setValidator(String inputNumber) {
-		inputNumStr = inputNumber;
-		inputNumberArr = inputNumStr.split("");
-		inputNumStrList = Arrays.asList(inputNumberArr);
-		inputNumStrSet = new HashSet<>(inputNumStrList);
-		
-		isContainZero();
-		isThreeNumber();
-		isDuplicate();
+		isNumeric(inputNumber);
+		isContainZero(inputNumber);
+		isThreeNumber(inputNumber);
+		isDuplicate(inputNumber);
 	}
-	
-	private static void isContainZero() {
-		if(inputNumStr.contains("0")) {
+
+	private static void isNumeric(String inputNumber) {
+		if(!inputNumber.matches(NUMERIC)) {
+			throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+		}
+	}
+
+	private static void isContainZero(String inputNumber) {
+		if(inputNumber.contains("0")) {
 			throw new IllegalArgumentException("0은 입력할 수 없습니다.");
 		}
 	}
 	
-	private static void isThreeNumber() {
-		if(inputNumStr.length() != BaseBallProperties.LENGTH_OF_NUMBER) {
+	private static void isThreeNumber(String inputNumber) {
+		if(inputNumber.length() != BaseBallProperties.LENGTH_OF_NUMBER) {
 			throw new IllegalArgumentException("세 자리 정수를 입력해주세요");
 		}
 	}
 	
-	private static void isDuplicate() {
-		if(inputNumStrList.size() != inputNumStrSet.size()) {
-			throw new IllegalArgumentException("같은 숫자는 입력할 수 없습니다.");
-		}
-	}
-	
-	private static void isOne() {
-		if(inputNumStr.equals("1")) {
-			baseBallController.start();
+	private static void isDuplicate(String inputNumber) {
+		Set<String> checkSet = new HashSet<>(Arrays.asList(inputNumber.split("")));
+		if(checkSet.size() != BaseBallProperties.LENGTH_OF_NUMBER) {
+			throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.");
 		}
 	}
 }
