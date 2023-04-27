a33e1c750b19b2e912125e1fe85a1a8ff82d293e
Kim Chanho <theanswerkk@naver.com>
2023-04-27 14:55:58 +0900

    [add] 게임결과객체추가

--- src/main/java/wapago/baseball/model/Result.java
+++ src/main/java/wapago/baseball/model/Result.java
@@ -1,5 +1,20 @@
 package wapago.baseball.model;
 
 public class Result {
+    private int strike;
+    private int ball;
+
+    public Result(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
+    }
 
 }
