5ba8adb7312f7288c2ae6f007aa60210dfbcfcb0
Kim Chanho <theanswerkk@naver.com>
2023-04-27 14:59:45 +0900

    [mod]결과view수정

--- src/main/java/wapago/baseball/view/GameView.java
+++ src/main/java/wapago/baseball/view/GameView.java
@@ -1,5 +1,7 @@
 package wapago.baseball.view;
 
+import wapago.baseball.constant.BaseBallProperties;
+
 import java.util.Scanner;
 
 public class GameView {
@@ -13,6 +15,33 @@ public class GameView {
 		String inputNumber = scanner.next();
 		
 		return inputNumber; 
+	}
+
+	public int showResult(int[] result) {
+		int strike = result[0];
+		int ball = result[1];
+
+		if(strike != 0 && ball == 0) {
+			System.out.println(strike + "스트라이크");
+        }
+
+        if(strike == 0 && ball != 0) {
+			System.out.println(ball + "볼");
+        }
+
+		if(strike != 0 && ball != 0) {
+			System.out.println(strike + "스트라이크 " + ball + "볼");
+		}
+
+		if(strike == 0 && ball == 0) {
+			System.out.println("낫싱");
+		}
+
+		if(strike == BaseBallProperties.LENGTH_OF_NUMBER) {
+			System.out.println(BaseBallProperties.LENGTH_OF_NUMBER + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		}
+
+		return strike;
 	}
 	
 	public void askRestart() {
