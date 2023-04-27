238ad3e6a03fb932d4584874fbb813d736c4a0fc
Kim Chanho <theanswerkk@naver.com>
2023-04-27 15:00:39 +0900

    [mod]play()수정,isRestart()추가

--- src/main/java/wapago/baseball/controller/BaseBallController.java
+++ src/main/java/wapago/baseball/controller/BaseBallController.java
@@ -1,16 +1,15 @@
 package wapago.baseball.controller;
 
 import wapago.baseball.constant.BaseBallProperties;
-import wapago.baseball.model.Computer;
 import wapago.baseball.util.CreateRandomNumber;
 import wapago.baseball.util.InputValidator;
+import wapago.baseball.util.MatchWithRndNumber;
 import wapago.baseball.view.GameView;
 
 public class BaseBallController {
 	public static Integer[] randomNumber = new Integer[BaseBallProperties.LENGTH_OF_NUMBER];
 	
 	GameView gameView = new GameView();
-	Computer computer = new Computer();
 
 	public void start() {
 		randomNumber = CreateRandomNumber.createRandomNumber();
@@ -18,12 +17,28 @@ public class BaseBallController {
 	}
 	
 	public void play() {
-		gameView.requestInput();
-		String inputNumber = gameView.getInputNumber();
-		InputValidator.setValidator(inputNumber);
+		int strike = 0;
+
+		while(strike != 3) {
+			gameView.requestInput();
+			String inputNumber = gameView.getInputNumber();
+			InputValidator.playInputValidator(inputNumber);
+			int[] result = MatchWithRndNumber.setMatchWithRandomNumber(inputNumber);
+			strike = gameView.showResult(result);
+		}
+
+		isRestart();
 	}
 	
-	public void askRestart() {
+	public void isRestart() {
 		gameView.askRestart();
+		String inputRestart = gameView.getInputNumber();
+		InputValidator.restartInputValidator(inputRestart);
+
+		if(inputRestart.equals("1")) {
+			start();
+		}
+
+		System.exit(0);
 	}
 }
